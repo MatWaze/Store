9017372916504650c7eeb005ac62811a11a3b55c
MatWaze <coolmartun@gmail.com>
2024-10-06 23:44:02 +0400

    Added extension methods that gets data saved in local cache instead of loading it from database. not using it thouhg

---
+++ Infrastructure/LocalOrDatabase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Store.Infrastructure
+{
+    public static class LocalOrDatabase
+    {
+        public static IEnumerable<EntityEntry<T>> Local<T>(this DbContext context)
+            where T : class
+        {
+            return context.ChangeTracker.Entries<T>();
+        }
+
+        public static IEnumerable<EntityEntry<T>> Local<T>(this DbSet<T> set)
+            where T : class
+        {
+            if (set is InternalDbSet<T>)
+            {
+                Console.WriteLine("Local");
+                var svcs = (set as InternalDbSet<T>)
+                    .GetInfrastructure()
+                    .GetService<IDbContextServices>();
+                var ctx = svcs.CurrentContext.Context;
+                return Local<T>(ctx);
+            }
+            throw new ArgumentException("Invalid set", "set");
+        }
+
+        public static IQueryable<T> LocalOrDb<T>(this DbContext context) where T : class
+        {
+            var localResults = context.Set<T>().Local.Where(c => true);
+
+            if (localResults.Any() == true)
+            {
+                Console.WriteLine(localResults.Count());
+                Console.WriteLine("trying to get products from cache");
+                return localResults.AsQueryable();
+            }
+           
+            IQueryable<T> databaseResults = context.Set<T>().Where(c => true);
+            return databaseResults;
+        }
+
+    }
+}
