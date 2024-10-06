745a70c83e662d3ea01f19bff1f0e9504ae727ac
MatWaze <coolmartun@gmail.com>
2024-10-06 23:44:45 +0400

    Added method to upload file directly to a blob storage

--- Infrastructure/BlobStorageService.cs
+++ Infrastructure/BlobStorageService.cs
@@ -23,9 +23,44 @@ public class BlobStorageService
         using (var stream = file.OpenReadStream())
         {
             await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = file.ContentType });
+            stream.Close();
         }
 
         return blobClient.Uri.ToString();
+    }
+
+    public async Task<string> UploadFileFromPathAsync(string filePath)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+        await containerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+        string uniqueFileName = filePath;
+        BlobClient blobClient = containerClient.GetBlobClient(uniqueFileName);
+        if (await blobClient.ExistsAsync() == false)
+        {
+            using (var fileStream = File.OpenRead(filePath))
+            {
+                await blobClient.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = "application/x-yaml" });
+                fileStream.Close();
+            }
+        }
+        return blobClient.Uri.ToString();
+    }
+
+    public async Task<Stream> GetFileStreamAsync(string fileName)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+        Console.WriteLine(fileName);
+
+        if (!await blobClient.ExistsAsync())
+        {
+            throw new FileNotFoundException($"File '{fileName}' does not exist in blob storage.");
+        }
+
+        BlobDownloadInfo download = await blobClient.DownloadAsync();
+        return download.Content; // This is a stream
     }
 
     public async Task DeleteFileAsync(string fileUrl)
