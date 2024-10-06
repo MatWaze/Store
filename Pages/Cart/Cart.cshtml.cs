332ae0db11f331772749194491827e32b87199f4
MatWaze <coolmartun@gmail.com>
2024-10-06 23:42:37 +0400

    Added Layout to Cart. Added react component that shows updates of a cart

--- Pages/Cart/Cart.cshtml.cs
+++ Pages/Cart/Cart.cshtml.cs
@@ -3,33 +3,33 @@ using Microsoft.AspNetCore.Mvc;
 using Store.Infrastructure;
 using Store.Models;
 using Microsoft.AspNetCore.Http;
+using NuGet.Protocol.Core.Types;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Globalization;
 
 namespace Store.Pages.Cart
 {
     public class CartModel : PageModel
     {
         private readonly IProductRepository repo;
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public CartModel(IProductRepository rep, Models.Cart cartService,
-            IHttpContextAccessor httpContextAccessor)
+        public CartModel(IProductRepository rep, Models.Cart cartService)
         {
             repo = rep;
             Cart = cartService;
-            _httpContextAccessor = httpContextAccessor;
+           
         }
 
         public Models.Cart? Cart { get; set; }
         public string ReturnUrl { get; set; } = "/";
 
-        public HttpRequest Request => _httpContextAccessor.HttpContext.Request;
 
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
         }
 
-        public IActionResult OnPost(string ebayProductId, string returnUrl)
+        public IActionResult OnPostAddToCart(string ebayProductId, string returnUrl)
         {
             Product? product = repo.Products
                 .FirstOrDefault(p => p.EbayProductId == ebayProductId.ToString());
@@ -37,19 +37,47 @@ namespace Store.Pages.Cart
             {
                 if (product.Quantity > 0)
                 {
-                    //product.Quantity--;
-                    //repo.SaveProduct(product);
-                    //product.Deleted = true;
                     CartLine? cl = Cart.Lines.FirstOrDefault(c => c.Product.ProductId == product.ProductId);
                     if (cl == null || cl.Quantity < product.Quantity)
                     {
-                        Cart.AddItem(product, 1, Request); // Add to cart 
-                    }
+                        Cart.AddItem(product, 1); // Add to cart
+                        var totalItems = Cart.Lines.Sum(cl => cl.Quantity); // Total items in the cart
+                        var totalValue = Cart.Lines.Sum(cl => cl.Product.Price * cl.Quantity); // Total cart value
 
+                        return new JsonResult(new
+                        {
+                            success = true,
+                            totalItems = totalItems,
+                            totalValue = totalValue.ToString("c", CultureInfo.GetCultureInfo("en-US")) // Format total value
+                        });
+                    }
                 }
             }
-            return RedirectToPage(new { returnUrl = returnUrl });
+            return new JsonResult(new { success = false });
         }
+
+
+        //public IActionResult OnPost(string ebayProductId, string returnUrl)
+        //{
+        //    Product? product = repo.Products
+        //        .FirstOrDefault(p => p.EbayProductId == ebayProductId.ToString());
+        //    if (product != null)
+        //    {
+        //        if (product.Quantity > 0)
+        //        {
+        //            //product.Quantity--;
+        //            //repo.SaveProduct(product);
+        //            //product.Deleted = true;
+        //            CartLine? cl = Cart.Lines.FirstOrDefault(c => c.Product.ProductId == product.ProductId);
+        //            if (cl == null || cl.Quantity < product.Quantity)
+        //            {
+        //                Cart.AddItem(product, 1); // Add to cart 
+        //            }
+
+        //        }
+        //    }
+        //    return RedirectToPage(new { returnUrl = returnUrl });
+        //}
 
         public IActionResult OnPostRemove(long productId, string returnUrl)
         {
@@ -62,7 +90,7 @@ namespace Store.Pages.Cart
                 //product.Quantity += cart.Quantity;
                 //repo.SaveProduct(product);
                 //product.Deleted = false;
-                Cart.RemoveLine(product, Request);
+                Cart.RemoveLine(product);
             }
             return RedirectToPage(new { returnUrl = returnUrl });
         }
