153f821c683da1b6a24fbf4383e138fd7da3d081
MatWaze <coolmartun@gmail.com>
2024-10-06 23:40:48 +0400

    Checkout with YooKassa functions normally now

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using System.Text;
 using Braintree;
 using Microsoft.AspNetCore.Identity;
@@ -6,13 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Store.Infrastructure;
 using Store.Models;
 using Yandex.Checkout.V3;
-using System.Web;
 using Newtonsoft.Json.Linq;
-using NuGet.Protocol;
-using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
-using Azure.Core;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Store.Controllers;
@@ -23,51 +18,55 @@ public class OrderController : Controller
 {
     public IBraintreeService brainService;
     public IOrderRepository repo;
+    public HttpClient httpClient;
     public IProductRepository prodRepo;
     public Cart cart;
+    private IConfiguration config;
     private UserManager<ApplicationUser> userManager;
-    private Client client;
-    public string? YooAccessToken;
 
     public OrderController(IBraintreeService braintreeService,
-        IOrderRepository repository, 
+        IOrderRepository repository,
         IProductRepository productRepository,
         Cart cartService,
-        UserManager<ApplicationUser> usrMgr)
-    {
+        UserManager<ApplicationUser> usrMgr,
+        IConfiguration configuration,
+        HttpClient client)
+    {   
         brainService = braintreeService;
         repo = repository;
         prodRepo = productRepository;
         cart = cartService;
         userManager = usrMgr;
-	}
+        config = configuration;
+        httpClient = client;
+    }
 
-    public async Task<string> GetUserYooAccessToken()
+    public async Task<string?> GetUserYooAccessToken()
     {
         var user = await userManager.GetUserAsync(User);
-        string tok = user.YooKassaAccessToken;
+        string? tok = user?.YooKassaAccessToken;
         return tok;
     }
 
-
-    public IActionResult New()
+    public async Task<IActionResult> New()
     {
+        ViewBag.User = await userManager.GetUserAsync(User);
         return View(new Order
         {
             Lines = cart.Lines.ToArray(),
         });
     }
 
-    public IActionResult YooKassaPayment(int orderId, string accessToken)
+    public async Task<IActionResult> YooKassaPayment(int orderId, string accessToken)
     {
-        ViewBag.ConfirmationToken = YooToken(orderId, accessToken);
+        CreateYooWebHook("waiting_for_capture", accessToken);
+        ViewBag.ConfirmationToken = await YooToken(orderId, accessToken);
         Console.WriteLine("Confirmation token: " + ViewBag.ConfirmationToken);
         return View("YooKassaCheckout", orderId);
     }
 
     public IActionResult BraintreePayment(int orderId)
     {
-        Console.WriteLine("id: " + orderId);
         Order? order = repo.Orders.FirstOrDefault(o => o.OrderID == orderId);
 
         if (order == null)
@@ -110,7 +109,7 @@ public class OrderController : Controller
             {
                 if (productIdsInOrder.Contains(prod.ProductId))
                 {
-                    CartLine? line = cart.Lines.FirstOrDefault(cl => cl.Product.ProductId == prod.ProductId);
+                    CartLine? line = cart.Lines.Find(cl => cl.Product.ProductId == prod.ProductId);
                     Console.WriteLine("line quan: " + line.Quantity);
                     if (prod.Quantity > 0)
                     {
@@ -124,8 +123,6 @@ public class OrderController : Controller
 				}
 			}
 			repo.SaveOrder(order);
-            cart.Clear(Request);
-            //repo.SaveOrder(order);
             return RedirectToPage("/Completed",
                 new { orderId = order.OrderID });
         }
@@ -147,7 +144,7 @@ public class OrderController : Controller
         }
     }
 
-    private NewReceipt CreateYooReceipt()
+    public async Task<NewReceipt> CreateYooReceipt()
     {
         List<ReceiptItem> receiptItems = new();
         foreach (var item in cart.Lines)
@@ -163,12 +160,13 @@ public class OrderController : Controller
             };
             receiptItems.Add(receiptItem);
         }
+        var user = await userManager.GetUserAsync(User);
         NewReceipt newReceipt = new NewReceipt
         {
             Customer = new Yandex.Checkout.V3.Customer
             {
-                FullName = "Матевос Амазарян Агванович",
-                Email = "coolmartun@gmail.com"
+                FullName = user.FullName,
+                Email = user.Email
             },
             Items = receiptItems,
             Send = true,
@@ -179,10 +177,9 @@ public class OrderController : Controller
     public async Task<IActionResult> Auth2(int orderId)
     {
         ApplicationUser? user = await userManager.GetUserAsync(User);
-        if (user.YooKassaAccessToken == null)
+        if (user?.YooKassaAccessToken == null /* or expired */)
         {
-            string clientId = "uhedu6u4q1b6mp2tvimjads6bng8s72q";
-            string apiUrl = $"https://yookassa.ru/oauth/v2/authorize?response_type=code&client_id={clientId}&state=test-user";
+            string apiUrl = $"https://yookassa.ru/oauth/v2/authorize?response_type=code&client_id={config["YooKassaApi:ClientId"]}&state=test-user";
             return Redirect(apiUrl);
 		}
         else
@@ -201,42 +198,37 @@ public class OrderController : Controller
             return RedirectToAction("Error");
         }
         string url = "https://yookassa.ru/oauth/v2/token";
-        string clientId = "uhedu6u4q1b6mp2tvimjads6bng8s72q";
-        string clientSecret = "Fqd60rf0uCGZxGTPnPX6QAFx1YN6BUeOFTTYnfLKYIvOyOx1v2T0PnGvXwWtG3pm";
-        using (var client = new HttpClient())
+       
+        var byteArray = Encoding
+            .ASCII.GetBytes($"{config["YooKassaApi:ClientId"]}:{config["YooKassaApi:ClientSecret"]}");
+
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+        var requestData = new Dictionary<string, string>
         {
-            var byteArray = Encoding.ASCII.GetBytes($"{clientId}:{clientSecret}");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-
-            var requestData = new Dictionary<string, string>
-                {
-                    { "grant_type", "authorization_code" },
-                    { "code", code }
-                };
-            var content = new FormUrlEncodedContent(requestData);
-
-            HttpResponseMessage response = await client.PostAsync(url, content);
-            if (response.IsSuccessStatusCode)
-            {
-                var responseString = await response.Content.ReadAsStringAsync();
-                var responseData = JObject.Parse(responseString);
+            { "grant_type", "authorization_code" },
+            { "code", code }
+        };
+        var content = new FormUrlEncodedContent(requestData);
+        HttpResponseMessage response = await httpClient.PostAsync(url, content);
+        if (response.IsSuccessStatusCode)
+        {
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseData = JObject.Parse(responseString);
                 
-                ApplicationUser? user = await userManager.GetUserAsync(User);
-                user.YooKassaAccessToken = responseData["access_token"]?.ToString();
-                await userManager.UpdateAsync(user);
-                Order? ord = await repo.Orders.FirstOrDefaultAsync(o => o.UserId == user.Id);
-                
-                Console.WriteLine($"Access Token: {user.YooKassaAccessToken}");
-				return RedirectToAction("YooKassaPayment", 
-                    new { orderId = ord?.OrderID, accessToken = user.YooKassaAccessToken });
-            }
-            else
-            {
-                // Handle error response
-                string errorResponse = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Error: {errorResponse}");
-                return RedirectToAction("Error"); // Redirect to an error page
-            }
+            ApplicationUser? user = await userManager.GetUserAsync(User);
+            user.YooKassaAccessToken = responseData["access_token"]?.ToString();
+            await userManager.UpdateAsync(user);
+
+            Order? ord = repo.Orders.FirstOrDefault(o => o.UserId == user.Id);
+			return RedirectToAction("YooKassaPayment", 
+                new { orderId = ord?.OrderID, accessToken = user.YooKassaAccessToken });
+        }
+        else
+        {
+            string errorResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Error: {errorResponse}");
+            return RedirectToAction("Error");
         }
     }
 
@@ -245,7 +237,7 @@ public class OrderController : Controller
         return RedirectToAction("Index", "Home");
     }
 
-    private string YooToken(int orderId, string accessToken)
+    private async Task<string> YooToken(int orderId, string accessToken)
     {
         Order? order = repo.Orders.FirstOrDefault(o => o.OrderID == orderId);
         decimal amount = Math.Round(cart.Lines.Sum(e => e.Product.Price * e.Quantity), 2);
@@ -253,24 +245,30 @@ public class OrderController : Controller
         {
             Amount = new Amount { Value = amount, Currency = "RUB" },
             Confirmation = new Confirmation { Type = ConfirmationType.Embedded },
-            Receipt = CreateYooReceipt(),
-            Metadata = new Dictionary<string, string> { { "OrderID", orderId.ToString() } },
+            Receipt = await CreateYooReceipt(),
+            Metadata = new Dictionary<string, string> 
+            { 
+                { "OrderID", orderId.ToString() },
+                { "AccessToken", accessToken }
+            },
         };
-        client = new Client(accessToken: accessToken);
-        Payment payment = client.CreatePayment(newPayment);
+        Console.WriteLine("access token: " + accessToken);
+        Client yooClient = new Client(accessToken: accessToken);
+        Payment payment = yooClient.CreatePayment(newPayment);
         //var user = await userManager.GetUserAsync(User);
         order.PaymentId = payment.Id;
-
+        Console.WriteLine($"Conf token: {payment.Confirmation.ConfirmationToken}");
         repo.SaveOrder(order);
         var confirmationToken = payment.Confirmation.ConfirmationToken;
         return confirmationToken;
     }
 
-    public async Task<IActionResult> Note()
+    [HttpPost]
+    [AllowAnonymous]
+    [IgnoreAntiforgeryToken]
+    [Route("Order/Notification")]
+    public async Task<IActionResult> Notification()
     {
-        string accessToken = await GetUserYooAccessToken();
-        client = new Client(accessToken: accessToken);
-        AsyncClient asyncClient = client.MakeAsync();
         // Enable buffering to allow multiple reads of the request body
         Request.EnableBuffering();
 
@@ -281,44 +279,41 @@ public class OrderController : Controller
             body = await reader.ReadToEndAsync();
             // Reset the stream position to allow further reading if necessary
             Request.Body.Position = 0;
+            reader.Close();
         }
         var notification = Client.ParseMessage(Request.Method, Request.ContentType, body);
-
-        if (notification is PaymentWaitingForCaptureNotification paymentWaitingForCaptureNotification)
+        if (notification is PaymentWaitingForCaptureNotification captureNote)
         {
-            Payment payment = paymentWaitingForCaptureNotification.Object;
-
+            Payment payment = captureNote.Object;
+            Console.WriteLine("Waiting for capture");
             if (payment.Paid)
             {
-                Console.WriteLine($"Got message: payment.id={payment.Id}, payment.paid={payment.Paid}");
+                Client yooClient = new Client(accessToken: payment.Metadata["AccessToken"]);
+                AsyncClient asyncClient = yooClient.MakeAsync();
 
                 Payment p = await asyncClient.CapturePaymentAsync(payment.Id);
-                Console.WriteLine(p.Metadata["OrderID"]);
                 int orderId = int.Parse(p.Metadata["OrderID"]);
-
                 Order? ord = repo.Orders.FirstOrDefault(o => o.OrderID == orderId);
-                ord.PaymentStatus = "Paid"; // setting status to paid
-				var productIdsInOrder = ord.Lines.Select(cl => cl.Product.ProductId).ToHashSet();
-				foreach (var prod in prodRepo.Products.ToList<Product>()) // changing product quantity or marking product as deleted
-				{
-					if (productIdsInOrder.Contains(prod.ProductId))
-					{
-						CartLine? line = cart.Lines.FirstOrDefault(cl => cl.Product.ProductId == prod.ProductId);
-						Console.WriteLine("line quan: " + line.Quantity);
-						if (prod.Quantity > 0)
-						{
-							prod.Quantity -= line.Quantity;
-						}
-						if (prod.Quantity == 0)
-						{
-							prod.Deleted = true;
-						}
-						prodRepo.SaveProduct(prod);
-					}
-				}
-				repo.SaveOrder(ord);
-				cart.Clear(Request);
-				string? receiptId = null;
+
+                Product[] productIdsInOrder = ord.Lines.Select(cl => cl.Product).ToArray();
+                Console.WriteLine("count: " + ord.Lines.Count());
+                foreach (Product prod in productIdsInOrder)
+                {
+                    if (prod.Quantity > 0)
+                    {
+                        CartLine? line = ord.Lines
+                            .ToList<CartLine>()
+                            .Find(cl => cl.Product.ProductId == prod.ProductId);
+                        prod.Quantity -= line.Quantity;
+                    }
+                    if (prod.Quantity == 0)
+                    {
+                        prod.Deleted = true;
+                    }
+                    prodRepo.SaveProduct(prod);
+                }
+                repo.SaveOrder(ord);
+                string? receiptId = null;
 
                 await foreach (var receipt in asyncClient.GetReceiptsAsync())
                 {
@@ -329,18 +324,37 @@ public class OrderController : Controller
                     }
                 }
                 Receipt? r = await asyncClient.GetReceiptAsync(receiptId);
-                if (r != null)
-                {
-                    Console.WriteLine("Receipt found: " + r.ToJson());
-                }
-                else
-                {
-                    Console.WriteLine("Receipt not found");
-                }
                 return Ok();
             }
         }
         return BadRequest();
+    }
+
+    public async void CreateYooWebHook(string notificationEvent, string accessToken)
+    {
+        string url = "https://api.yookassa.ru/v3/webhooks";
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        HttpResponseMessage getResponse = await httpClient.GetAsync(url);
+
+        if (getResponse.IsSuccessStatusCode)
+        {
+            string responseString = await getResponse.Content.ReadAsStringAsync();
+            JObject? responseJson = JObject.Parse(responseString);
+            if (responseJson["items"].HasValues == false)
+            {
+                var content = new
+                {
+                    @event = "payment." + notificationEvent,
+                    url = "https://iparts.me/Order/Notification" 
+                };
+                Console.WriteLine($"Creating WebHook for {notificationEvent}");
+                httpClient.DefaultRequestHeaders.Add("Idempotence-Key", Guid.NewGuid().ToString());
+                var resp = await httpClient.PostAsJsonAsync(url, content);
+                httpClient.DefaultRequestHeaders.Remove("Idempotence-Key");
+            }
+            else
+                Console.WriteLine($"WebHook for {notificationEvent} already exists");
+        }
     }
 
     [HttpPost]
@@ -351,13 +365,13 @@ public class OrderController : Controller
             ModelState.AddModelError("",
                 "Sorry, your cart is empty!");
         }
-
         if (ModelState.IsValid)
         {
             order.Lines = cart.Lines;
             if (order.Lines.Count() > 0)
             {
                 var user = await userManager.GetUserAsync(User);
+                Console.WriteLine(user?.UserName ?? "null name");
                 order.UserId = user.Id;
                 order.Lines = cart.Lines;
                 order.PaymentStatus = "Pending";
