bd2dcb39f38eb560195ebeda6b60591ccaa82039
MatWaze <coolmartun@gmail.com>
2024-11-01 23:11:55 +0400

    Separated functionality into two controllers. One is for showing products from db, the other for ebay notification endpoint

---
+++ Controllers/AccountApiController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Store.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+
+namespace Store.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountApiController : ControllerBase
+    {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+
+        public AccountApiController(
+            UserManager<ApplicationUser> usrMgr,
+            SignInManager<ApplicationUser> sgnMgr
+        )
+        {
+            userManager = usrMgr;
+            signInManager = sgnMgr;
+        }
+
+        [HttpPost("token")]
+        public async Task<IActionResult> Token(
+            [FromBody] Credentials credentials
+        )
+        {
+            string? username = credentials.Username;
+            string? password = credentials.Password;
+
+            ApplicationUser? user = await userManager.FindByNameAsync(username);
+
+            if (user != null && (await signInManager
+                .CheckPasswordSignInAsync(user, password, true))
+                .Succeeded
+            )
+            {
+                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+                byte[] secret = Encoding.ASCII
+                    .GetBytes(Environment.GetEnvironmentVariable("jwtSecret")!);
+                SecurityTokenDescriptor desk = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, username)
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(24),
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(secret),
+                        SecurityAlgorithms.HmacSha256Signature
+                    )
+                };
+
+                SecurityToken token = handler.CreateToken(desk);
+                return Ok(new
+                {
+                    success = true,
+                    token = handler.WriteToken(token)
+                });
+            }
+            return Unauthorized();
+        }
+
+        public class Credentials
+        {
+            public string Username { get; set; } = String.Empty;
+            public string Password { get; set; } = String.Empty;
+        }
+    }
+}
---
+++ Controllers/EbayNotificationApiController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EbayNotificationApiController : ControllerBase
+    {
+        private string VerificationToken = "a94cbd68e463cb9780e2008b1f61986110a5fd0ff8b99c9cba15f1f802ad65";
+        private string EndpointUrl = "https://eafd-83-139-27-248.ngrok-free.app/api/products";
+        private string NotificationsPath = "./notifications/";
+
+        [HttpGet]
+        public IActionResult VerifyEndpoint([FromQuery] string challenge_code)
+        {
+            // Compute the SHA-256 hash
+            IncrementalHash sha256 = IncrementalHash.CreateHash(HashAlgorithmName.SHA256);
+            sha256.AppendData(Encoding.UTF8.GetBytes(challenge_code));
+            sha256.AppendData(Encoding.UTF8.GetBytes(VerificationToken));
+            sha256.AppendData(Encoding.UTF8.GetBytes(EndpointUrl));
+            byte[] bytes = sha256.GetHashAndReset();
+            var hashHex = BitConverter.ToString(bytes).Replace("-", string.Empty).ToLower();
+            Console.WriteLine(hashHex);
+            // Create the response
+            var response = new
+            {
+                challengeResponse = hashHex
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HandleNotification()
+        {
+            // Read the request body
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var body = await reader.ReadToEndAsync();
+
+            // Save the notification to a file
+            var currentDtNow = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            var filePath = Path.Combine(NotificationsPath, $"{currentDtNow}_user_notification.txt");
+            await System.IO.File.WriteAllTextAsync(filePath, body);
+
+            // Return a JSON response
+            var response = new
+            {
+                status = "Notification received"
+            };
+            return new ContentResult
+            {
+                Content = System.Text.Json.JsonSerializer.Serialize(response),
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+    }
+}
