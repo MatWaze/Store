301167c03bff88ac58253c07c1215dd86aab0935
MatWaze <coolmartun@gmail.com>
2024-11-21 19:57:07 +0400

    removed yandex

--- Yandex.Checkout.V3/Leg.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yandex.Checkout.V3;
-
-// ReSharper disable once ClassNeverInstantiated.Global
-public class Leg
-{
-    public string DepartureAirport { get; set; }
-    public string DestinationAirport { get; set; }
-    public DateTime DepartureDate { get; set; }
-}
--- Yandex.Checkout.V3/Passenger.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Yandex.Checkout.V3;
-
-// ReSharper disable once ClassNeverInstantiated.Global
-public class Passenger
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
