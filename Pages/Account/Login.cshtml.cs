d72df0987c1f9da6bf9c68d2dbcdddf61869da72
MatWaze <coolmartun@gmail.com>
2024-11-21 15:40:15 +0400

    Localized error messages

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Store.Models;
+using Store.Resources.Models;
+using Store.Validation;
 
 namespace Store.Pages.Account
 {
-    public class LoginModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class LoginModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
 }
--- Pages/Account/Signup.cshtml.cs
+++ Pages/Account/Signup.cshtml.cs
@@ -1,12 +1,19 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Store.Infrastructure;
+using Store.Models;
+using Store.Resources.Models;
+using Store.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Store.Pages.Account
 {
-    public class SignupModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class SignupModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
 }
