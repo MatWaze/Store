950624774a28be79e4a0153954cc65eb91f8709d
MatWaze <coolmartun@gmail.com>
2024-11-27 02:41:07 +0400

    Made Yandex checkout a submodule

--- Yandex.Checkout.V3/IsExternalInit.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices;
-
-// https://stackoverflow.com/a/62656145/1291717
-[EditorBrowsable(EditorBrowsableState.Never)]
-// ReSharper disable once UnusedType.Global
-public class IsExternalInit{}
--- Yandex.Checkout.V3/PayoutDestination.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yandex.Checkout.V3;
-
-// ReSharper disable once ClassNeverInstantiated.Global
-public class PayoutDestination
-{
-    public string Type { get; set; }
-        
-    public Card Card { get; set; }
-}
