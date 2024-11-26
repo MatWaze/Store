a3929d054822d489254d45ef337a7aaffc7cd5a2
MatWaze <coolmartun@gmail.com>
2024-11-27 02:42:21 +0400

    Localized cart and GetItem page

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode, useState, useEffect } from 'react';
+﻿import { StrictMode, useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { useTranslation } from 'react-i18next';
-import './i18n'; // Import i18n configuration
 import SpeechToText from './SpeechToText.jsx';
 
-function CartButton() {
-    const { t } = useTranslation(); // Hook for localization
+function CartButtonEn() {
     const [cart, setCart] = useState({
         totalItems: 0,
         totalValue: '$0.00'
@@ -36,7 +33,7 @@ function CartButton() {
     return (
         <div className="">
             <small className="navbar-text">
-                <b>{t("Your cart")}:</b> {cart.totalItems} {t("item(s)")} {cart.totalValue}
+                <b>{"Your cart"}:</b> {cart.totalItems} {"item(s)"} {cart.totalValue}
             </small>
             <a className="btn btn-md btn-secondary navbar-btn mt-2"
                 href={`/Cart/Cart?returnUrl=${encodeURIComponent(returnUrl)}`}>
@@ -46,10 +43,60 @@ function CartButton() {
     );
 }
 
+function CartButtonRu() {
+    const [cart, setCart] = useState({
+        totalItems: 0,
+        totalValue: '$0.00'
+    });
+
+    const updateCart = (totalItems, totalValue) => {
+        setCart({
+            totalItems: parseInt(totalItems, 10),
+            totalValue: totalValue
+        });
+    };
+    const [returnUrl, setReturnUrl] = useState('');
+
+    useEffect(() => {
+        window.updateCart = updateCart;
+        const currentUrl = window.location.href;
+        setReturnUrl(currentUrl);
+        const cartButtonElement = document.querySelector('#cart-button-ru');
+        const totalItems = cartButtonElement.getAttribute('data-cart-items');
+        const totalValue = cartButtonElement.getAttribute('data-cart-total');
+        setCart({
+            totalItems: parseInt(totalItems, 10),
+            totalValue: totalValue
+        });
+    }, []);
+
+    return (
+        <div className="">
+            <small className="navbar-text">
+                <b>{"Ваша корзина"}:</b> {cart.totalItems} {"товар(ов)"} {cart.totalValue}
+            </small>
+            <a className="btn btn-md btn-secondary navbar-btn mt-2"
+                href={`/Cart/Cart?returnUrl=${encodeURIComponent(returnUrl)}`}>
+                <i className="fa fa-shopping-cart"></i>
+            </a>
+        </div>
+    );
+}
+
+
+
 const cartButtonElement = document.querySelector("#cart-button");
+const cartButtonElementRu = document.querySelector("#cart-button-ru");
+
 if (cartButtonElement) {
     createRoot(cartButtonElement).render(
-        <CartButton />
+        <CartButtonEn />
+    );
+}
+
+if (cartButtonElementRu) {
+    createRoot(cartButtonElementRu).render(
+        <CartButtonRu />
     );
 }
 
