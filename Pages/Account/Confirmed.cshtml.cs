83b2aef4807c2990a3bb2781c417fd142b362e1e
MatWaze <coolmartun@gmail.com>
2024-12-04 01:34:40 +0400

    Localized Access Denied page. Signing in user inside sign up page and redirecting user to confirm page

---
+++ Pages/Account/Confirmed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Store.Pages.Account
+{
+    public class ConfirmedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
